bb212ab7f1d7fb7b7acf2bb3a0d3932f0f391012
Nate Lovell <Natelovell99@gmail.com>
2021-08-16 15:59:01 -0600

    Create Create Adjustment Layers.jsx

---
+++ Create Adjustment Layers.jsx
@@ -0,0 +1,9 @@
+﻿function getExtensionPath(path) {
+    adjustmentLayerPRPath = path+"/adjustmentLayer.prproj";
+    adjustmentLayerPRPath = adjustmentLayerPRPath.replace(/%20/g, " ");
+    
+    var adjustmentItem;
+             if(adjustmentItem == null) {
+                app.project.importSequences(adjustmentLayerPRPath, ["e70122dd-d6fe-4d37-bd46-9a3769bac51d"]);
+                }
+    }
